f064d401e739e7d6ca57be8802d61f11d9759475
RudySeo <gun100491@gmail.com>
2026-05-05 14:40:42 +0900

    .

---
+++ 서동권/week04/120812. 최빈값 구하기/Solution.py
@@ -0,0 +1,10 @@
+def solution(array):
+    num = 0
+    data = 0
+    for i in set(array):
+        if array.count(i) > num:
+            num = array.count(i)
+            data = i
+        elif array.count(i) == num:
+            data = -1
+    return data
